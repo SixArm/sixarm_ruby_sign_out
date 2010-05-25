3cfac6f066d1a7bfa35a28e74990b0545acbe0b5
Joel Parker Henderson <joel@sixarm.com>
2010-05-24 21:49:38 -0700

    first commit

---
+++ lib/sixarm_ruby_sign_out.rb
@@ -0,0 +1,109 @@
+=begin rdoc
+
+= SixArm Ruby Gem: Sign Out
+
+Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
+License:: CreativeCommons License, Non-commercial Share Alike
+License:: LGPL, GNU Lesser General Public License
+
+Sign Out simple abstract framework.
+
+You can include this module in your application controller,
+then your controller can define any of these concrete methods. 
+
+This provides one top-level method:
+  sign_out(options=nil) => true for success, false for failure
+
+The top level method will call mid-level methods
+that you will define in your own controller.
+
+sign_out calls:
+  sign_out_attempt(options=nil), e.g. unset the current user
+  sign_out_success(options=nil), e.g. redirect to a goodbye page
+  sign_out_failure(options=nil), e.g. flash warning help message
+
+
+== AuthLogic Example
+
+AuthLogic provides this example:
+  def destroy
+    current_user_session.destroy
+    redirect_to new_user_session_url
+  end
+
+The same example written using Sign Out to provide better structure and security:
+  
+  def sign_out_attempt
+    current_user_session.destroy
+    current_user_session.frozen? or raise SecurityError, "Problem with sign out ... ", caller
+  end
+
+  def sign_out_success
+    redirect_to new_user_session_url
+  end
+
+  def sign_out_failure
+    # The AuthLogic example doesn't handle this case unfortunately;
+    # we should handle it because it indicates something is amiss.
+  end
+
+=end
+
+module SignOut
+
+
+ # The sign_out method calls:
+ # - sign_out_attempt
+ # - sign_out_success (if sign_out_attempt returns true)
+ # - sign_out_failure (if sign_out_attempt returns false or raises a SecurityError)
+
+ def sign_out(options=nil)
+  begin
+   sign_out_attempt(options) or raise SecurityError
+   sign_out_success(options)
+   return true
+  rescue SecurityError
+   sign_out_failure(options)
+   return false
+  end
+ end
+
+
+ # The sign_out method calls this first.
+ #
+ # You override this method to e.g.:
+ # - prompt the user to save any of his open work
+ # - clear the current user variable
+ # - clear the current user id in the session 
+ #
+ # If this method succeeds, then control goes to sign_out_success.
+ #
+ # If this method raises an error, then control goes to sign_out_failure.
+
+ def sign_out_attempt(options=nil)
+ end
+
+
+ # The sign_out method calls this second,
+ # iff sign_out_attempt succeeds.
+ #
+ # You override this method to e.g.:
+ # - show the user a flash notice message saying "Goodbye..."
+ # - redirect to the application home page
+ 
+ def sign_out_success(options=nil)
+ end
+
+
+ # The sign_out method calls this second,
+ # iff sign_out_attempt raises an exception.
+ #
+ # You override this method to e.g.:
+ # - show the user a flash warning message saying "Problem..."
+ # - redirect to a help page with a way to report the problem
+ 
+ def sign_out_failure(options=nil)
+ end
+
+end
---
+++ test/sixarm_ruby_sign_out_test.rb
@@ -0,0 +1,47 @@
+require 'test/unit'
+require 'sixarm_ruby_sign_out'
+
+class SignOutTest < Test::Unit::TestCase
+
+ include SignOut
+
+ RIGHT=:foo
+ WRONG=:bar
+
+ def setup
+  @sign_out_attempt_seen=false
+  @sign_out_success_seen=false
+  @sign_out_failure_seen=false
+ end
+
+ def test_sign_out_right
+  x=sign_out(RIGHT)
+  assert_equal(true,x,"sign_out x:#{x}")
+  assert( @sign_out_attempt_seen,"sign_out_attempt_seen")
+  assert( @sign_out_success_seen,"sign_out_success_seen")
+  assert(!@sign_out_failure_seen,"sign_out_failure_seen")
+ end
+
+ def test_sign_out_wrong
+  x=sign_out(WRONG)
+  assert_equal(false,x,"sign_out x:#{x}")
+  assert( @sign_out_attempt_seen,"sign_out_attempt_seen")
+  assert(!@sign_out_success_seen,"sign_out_success_seen")
+  assert( @sign_out_failure_seen,"sign_out_failure_seen")
+ end
+
+ def sign_out_attempt(options=nil)
+  @sign_out_attempt_seen=true
+  if options==WRONG then raise SecurityError end
+  return options==RIGHT
+ end
+
+ def sign_out_success(options=nil)
+  @sign_out_success_seen=true
+ end
+
+ def sign_out_failure(options=nil)
+  @sign_out_failure_seen=true
+ end
+
+end
